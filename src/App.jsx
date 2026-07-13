803cfce70d311ecc3db691749f084635213634f7
xxaldine <amoukiesredouane@gmail.com>
2026-07-13 20:52:17 +0100

    fixed deployment url

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
